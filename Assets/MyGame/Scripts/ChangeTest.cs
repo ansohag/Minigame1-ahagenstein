ac8c92788304774ff22dff32243a5e24d5d37215
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2019-05-08 15:29:09 +0200

    testing random script added and random functions test

--- Assets/MyGame/Scripts/ChangeTest.cs
+++ Assets/MyGame/Scripts/ChangeTest.cs
@@ -10,17 +10,22 @@ public class ChangeTest : MonoBehaviour {
 
     public Text myText;
     public TextMeshProUGUI myTMPText;
+    public TestingRandom myScript;
 
     private int countPresses = 0;
 
 
     private System.Random rnd = new System.Random();
+    private System.Random seedRnd = new System.Random(123);
+
+
     // Use this for initialization
     void Start () {
         myText.text = "hallooo";
         myTMPText.text = "nochmal hallooo";
         myText.fontSize = 30;
-        Debug.Log(rnd.Next(0, 5));
+        Debug.Log("randommmmm " + rnd.Next(0, 5));
+        Debug.Log("randommmmm " + UnityEngine.Random.Range(0, 1));
     }
    
 
@@ -32,7 +37,9 @@ public class ChangeTest : MonoBehaviour {
         countPresses = countPresses + 1;
         Debug.Log("counts: " + countPresses);
         myText.text = countPresses.ToString();
-     }
+        Debug.Log("randommmmm seed " + seedRnd.Next(0, 5));
+        myScript.ChangeRandomCube();
+    }
 
     // Update is called once per frame
     void Update () {
---
+++ Assets/MyGame/Scripts/TestingRandom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingRandom : MonoBehaviour {
+    public float rndVal;
+    public float rndVal2;
+    public int minValX = -6;
+    public int maxValX = 6;
+
+    private System.Random rndSystem = new System.Random();
+    // Use this for initialization
+    void Start () {
+        rndVal = rndSystem.Next(0, 10);
+        rndVal2 = UnityEngine.Random.Range(4, 100);
+        Debug.Log("System Random " + rndVal + " Unity Random " + rndVal2);
+
+
+    }
+    public void ChangeRandomCube()
+    {
+        gameObject.transform.position = new Vector3(rndSystem.Next(minValX, maxValX), 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
