923878de7270d93beea87edb60aa44d2f72611d6
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2019-06-18 08:31:09 +0200

    Refactoring Spawner Script

--- Assets/MyGame/Scripts/SpawnFlower1.cs
+++ Assets/MyGame/Scripts/SpawnFlower1.cs
@@ -7,21 +7,15 @@ public class SpawnFlower1 : MonoBehaviour
     public Flower1 flower1Prefab;
     public GameObject flowerParent;
 
-    private void Update()
-    {
-        //SpawnFl();
-    }
-
     IEnumerator Start()
     {
         while (true)
         {
+            //Flower1 wird alle 3 sekunden gespawned
             yield return new WaitForSeconds(3);
             SpawnFlowr1();
         }
-
     }
-
     private void SpawnFlowr1()
     {
         Flower1 flower1Clone = (Flower1)Instantiate(flower1Prefab, transform.position, transform.rotation);
@@ -30,8 +24,6 @@ public class SpawnFlower1 : MonoBehaviour
         flower1Clone.transform.SetParent(flowerParent.transform);
         flower1Clone.transform.localPosition = new Vector3(UnityEngine.Random.Range(-5f, 5f), flowerParent.transform.position.y, 0f);
         flower1Clone.GetComponent<Rigidbody2D>().velocity = new Vector2(UnityEngine.Random.Range(0, 0), UnityEngine.Random.Range(-5, -1));
-
     }
-
 }
 
