3cb8146460c1802bc5e05d4ca3c8e2c5c5f3ac57
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2019-06-18 08:28:05 +0200

    Refactoring Flower Scripts

--- Assets/MyGame/Scripts/CollectibleBad.cs
+++ Assets/MyGame/Scripts/CollectibleBad.cs
@@ -5,10 +5,6 @@ using TMPro;
 
 public class CollectibleBad : MonoBehaviour
 {
-    //private int score = 0;
-    //public TextMeshProUGUI pointsScore;
-    //public MyScore myScore;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Triggerevent " + collision.gameObject.name);
@@ -17,16 +13,12 @@ public class CollectibleBad : MonoBehaviour
         {
             ScoreManager.localScore -= 20;
             Debug.Log(ScoreManager.localScore);
-            //Debug.Log("score bee bad" + myScore);
-            //PointsScore.text = score.ToString();
-            //pointsScore.text = score.score.ToString();
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("CollisionEvent");
-
     }
 }
 
--- Assets/MyGame/Scripts/Flower1.cs
+++ Assets/MyGame/Scripts/Flower1.cs
@@ -7,11 +7,12 @@ public class Flower1 : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Flower wird destroyed wenn es den Collider Bottom berührt
         if (collision.gameObject.name == "ColliderBottom")
         {
             Destroy(gameObject);
         }
-
+        //Flower wird destroyed wenn es die biene berührt 
         if (collision.gameObject.name == "bee")
         {
             Destroy(gameObject);
